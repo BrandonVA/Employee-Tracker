641d39b10ea0a7d453d59dd23e62f972bca1965f
BrandonVA <codebva@gmail.com>
2020-11-21 14:44:26 -0800

    Added a prompt to collect selected role id and update it based on selected employees id.

--- prompts/setNewRole.js
+++ prompts/setNewRole.js
@@ -2,17 +2,57 @@ const inquirer = require('inquirer');
 
 module.exports = (connection) => {
 
-    connection.query('SELECT * FROM role;', (err, results) => {
-        console.table(results)
-        // inquirer.prompt([
-        //     {
-        //         type: 'input',
-        //         message: 'Whats the departments name?',
-        //         name: 'department_name',
-        //     },
-        // ]).then(department => {
-        //     console.log(department);
-        //     require('../db/dbCalls/addingData/newDepartment')(connection, department);
-        // })
+    connection.query('SELECT * FROM role; SELECT * FROM employee;', (err, results) => {
+        const roleData = results[0];
+        const employeeData = results[1];
+        console.table(roleData);
+        // console.table(employeeData);
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Whats employee do you want to assign a new role for?',
+                name: 'employee_id',
+                choices: function() {
+                    const listOfEmployees = []
+                    for (let i = 0; i < employeeData.length; i++) {
+                        listOfEmployees.push(`${employeeData[i].first_name} ${employeeData[i].last_name}`);
+                    }
+                    return listOfEmployees;
+                },
+                filter: function(val) {
+                    for (let i = 0; i < employeeData.length; i++) {
+                        let employee = `${employeeData[i].first_name} ${employeeData[i].last_name}`;
+                        if (val === employee) {
+                            val = employeeData[i].id;
+                            return val;
+                        }
+                    }
+                }
+            },
+            {
+                type: 'list',
+                message: 'What the new role you would like to assign',
+                name: 'newRole_id',
+                choices: function() {
+                    let roleChoices = [];
+                    for (let i = 0; i < roleData.length; i ++) {
+                        roleChoices.push(roleData[i].title);
+                    }
+                    return roleChoices;
+                },
+                filter: function(val) {
+                    for (let i = 0; i < roleData.length; i ++) {
+                        if (val === roleData[i].title) {
+                            val = roleData[i].id;
+                            return val;
+                        }
+                    }
+                }
+            }
+        ]).then(response => {
+            console.log(response);
+            require('../db/dbCalls/addingData/updateEmployeeRoles')(connection, response);
+        })
     })
+    // connection.end()
 }
