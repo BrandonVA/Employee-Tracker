e17dbaef3264c9939bc9f53c47aadf4e8cabcfa4
BrandonVA <codebva@gmail.com>
2020-11-20 17:25:38 -0800

    Call to the db that joins all the tables and displays that to the user.

---
+++ db/dbCalls/viewAllData.js
@@ -0,0 +1,27 @@
+const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, role.salary, employee.manager_id
+FROM employee
+JOIN role ON employee.role_id = role.id
+join department on role.department_id = department.id `
+
+module.exports = (connection) => {
+    connection.query(query, (err, results) => {
+        
+        connection.query('select * from employee', (err, res) => {
+            results.forEach(employee => {
+                if( employee.manager_id !== null) {
+                    // console.log(employee.manager_id)
+                    res.forEach(nestedEmployee => {
+                        if (nestedEmployee.id === employee.manager_id) {
+                            // console.log(`${nestedEmployee.id} is = ${employee.manager_id}`)
+                            employee.manager_id = `${nestedEmployee.first_name} ${nestedEmployee.last_name}`
+                        }
+                    })
+                }
+            });
+            // console.table(res);
+            console.table(results);
+            require('../../prompts/start')(connection);
+        })
+        // connection.end()
+    })
+}
