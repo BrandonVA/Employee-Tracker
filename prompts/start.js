23af7e57c90aec06d36d480194ac87af36600ab4
BrandonVA <codebva@gmail.com>
2020-11-20 17:28:43 -0800

    Added extra option for the inquirer prompt along with logic to excute code if its selected.

--- prompts/start.js
+++ prompts/start.js
@@ -1,1 +1,41 @@
-const inquirer = require('requirer');
+const inquirer = require('inquirer');
+
+
+
+module.exports =  async (connection) => {
+    // Promise.return(
+        // require('../db/dbCalls/viewAllData')(connection);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'test',
+                message: 'What would you like to do?',
+                choices: [
+                    'Search Employees',
+                    'Search Roles',
+                    'Search Departments',
+                    'View All Employees and data',
+                    'Exit'
+                ]
+            }
+        ]).then(response => {
+
+            let querySearch = response.test
+            if (querySearch === 'Search Employees') {
+                  console.log('searching db for employees');
+                  require('../db/dbCalls/getEmployees')(connection)
+            } else if (querySearch === 'Search Roles') {
+                console.log("hello 1");
+                require('../db/dbCalls/getRoles')(connection)
+            } else if (querySearch === 'Search Departments') {
+                console.log('Hello 2');
+                require('../db/dbCalls/getDepartments')(connection)
+            } else if ( querySearch === 'View All Employees and data') {
+                require('../db/dbCalls/viewAllData')(connection)
+            }else if (querySearch === 'Exit') {
+                connection.end()
+            }
+        })
+    // )
+
+}
