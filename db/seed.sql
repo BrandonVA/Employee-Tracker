c8c5b22008e373c807bcf17169901eaa4847e44d
BrandonVA <codebva@gmail.com>
2020-11-20 17:30:18 -0800

    Updated some employees and managers.

--- db/seed.sql
+++ db/seed.sql
@@ -1,16 +1,16 @@
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES ('Software');
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES ('Looney Bin');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Manager', 200000, 1);
+VALUES ('Software Lead', 200000, 1);
 INSERT INTO role (title, salary, department_id)
 VALUES ('Engineer', 100000, 1);
 INSERT INTO role (title, salary, department_id)
 VALUES ('Finding Neemo', 1000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Manager', 900000, 2);
+VALUES ('Looney Lead', 900000, 2);
 
 -- Employees
 INSERT INTO employee (first_name, last_name, role_id)
@@ -27,8 +27,8 @@ VALUES ('Sam', 'Wise', 2, 1);
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ('Willy', 'Wonka', 4);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Maddam', 'MadMan', 3, 2);
+VALUES ('Maddam', 'MadMan', 3, 5);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Old', 'Yeller', 3, 2);
+VALUES ('Old', 'Yeller', 3, 5);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Uncle', 'Sam', 3, 2);
+VALUES ('Uncle', 'Sam', 3, 5);
