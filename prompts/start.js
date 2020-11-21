fb5d0f6eb0e4063732e46d09347baa3d54103fd9
BrandonVA <codebva@gmail.com>
2020-11-20 17:44:19 -0800

    Removed if/else with a switch statment.

--- prompts/start.js
+++ prompts/start.js
@@ -21,20 +21,40 @@ module.exports =  async (connection) => {
         ]).then(response => {
 
             let querySearch = response.test
-            if (querySearch === 'Search Employees') {
-                  console.log('searching db for employees');
-                  require('../db/dbCalls/getEmployees')(connection)
-            } else if (querySearch === 'Search Roles') {
-                console.log("hello 1");
-                require('../db/dbCalls/getRoles')(connection)
-            } else if (querySearch === 'Search Departments') {
-                console.log('Hello 2');
-                require('../db/dbCalls/getDepartments')(connection)
-            } else if ( querySearch === 'View All Employees and data') {
-                require('../db/dbCalls/viewAllData')(connection)
-            }else if (querySearch === 'Exit') {
-                connection.end()
+
+            switch(querySearch) {
+                case 'Search Employees': 
+                    require('../db/dbCalls/getEmployees')(connection)
+                break;
+                case 'Search Roles' : 
+                    require('../db/dbCalls/getRoles')(connection);
+                break;
+                case 'Search Departments': 
+                    require('../db/dbCalls/getDepartments')(connection);
+                break;
+                case 'View All Employees and data': 
+                    require('../db/dbCalls/viewAllData')(connection);
+                break;
+                case 'Exit':
+                    connection.end();
+                break;
             }
+
+
+            // if (querySearch === 'Search Employees') {
+            //       console.log('searching db for employees');
+            //       require('../db/dbCalls/getEmployees')(connection)
+            // } else if (querySearch === 'Search Roles') {
+            //     console.log("hello 1");
+            //     require('../db/dbCalls/getRoles')(connection)
+            // } else if (querySearch === 'Search Departments') {
+            //     console.log('Hello 2');
+            //     require('../db/dbCalls/getDepartments')(connection)
+            // } else if ( querySearch === 'View All Employees and data') {
+            //     require('../db/dbCalls/viewAllData')(connection)
+            // }else if (querySearch === 'Exit') {
+            //     connection.end()
+            // }
         })
     // )
 
