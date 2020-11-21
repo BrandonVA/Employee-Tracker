5024789cb3d7fd38dd76ac0c58fbd975fdaea1a8
BrandonVA <codebva@gmail.com>
2020-11-21 12:57:27 -0800

    Cleaned up code.

--- db/dbCalls/getEmployees.js
+++ db/dbCalls/getEmployees.js
@@ -1,18 +1,28 @@
-module.exports = async (connection) => {
+const testing = async (connection) => {
     const query = `SELECT * FROM employee`
     
     return Promise.resolve(
         connection.query(query, (err, res) => {
             // let employees = [];
             console.table(res);
-            require('../../prompts/start')(connection);
+            
 
-            // res.forEach(item => employees.push(item.first_name) );
+            // res.forEach(item => {
+            //     let obj = {
+            //         first_name: item.first_name,
+            //         last_name: item.last_name,
+            //         role_id: item.role_id,
+            //         manager_id: item.manager_id,
+            //     }
+            //     employees.push(obj) 
+            // });
             // console.log(employees);
+            require('../../prompts/start')(connection);
+            // return employees;
     
         })
     )
 
-
-
 }
+
+module.exports = testing;
