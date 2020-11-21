f4538c8c245f5bb51573332b31b71afcee0ac195
BrandonVA <codebva@gmail.com>
2020-11-21 14:42:53 -0800

    Updated code to add change employe role_id based on selected employees id

--- db/dbCalls/addingData/updateEmployeeRoles.js
+++ db/dbCalls/addingData/updateEmployeeRoles.js
@@ -1,8 +1,9 @@
 module.exports = (connection, data) => {
-    const query = `UPDATE employee SET role_id = ${data}`
+    const {employee_id, newRole_id} = data;
+    const query = `UPDATE employee SET role_id = ${newRole_id} WHERE employee.id = ${employee_id}`
     connection.query(query, data, (err, res) =>{
         if (err) throw err;
-        console.log(res.affectedRows + "Employee Updated!\n");
-        // require('../../../prompts/start')(connection);
+        console.log(res.affectedRows + " Employee Updated!\n");
+        require('../../../prompts/start')(connection);
     })
 }
