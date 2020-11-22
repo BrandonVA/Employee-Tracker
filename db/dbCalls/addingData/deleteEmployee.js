b784d1b4387225b27f335425c7ff17af61931cde
BrandonVA <codebva@gmail.com>
2020-11-22 13:08:10 -0800

    Changed file location.

--- db/dbCalls/addingData/deleteEmployee.js
+++
@@ -1,8 +0,0 @@
-module.exports = (connection, data) => {
-    const query = `DELETE FROM employee WHERE id = ?`
-    connection.query(query, data, (err, res) =>{
-        if (err) throw err;
-        console.log(res.affectedRows + " Employee Removed\n");
-        require('../../../prompts/start')(connection);
-    })
-}
