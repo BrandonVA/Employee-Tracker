8324e6cfa71e41fcab3021995bc7583256215f8e
BrandonVA <codebva@gmail.com>
2020-11-27 16:41:17 -0800

    Added funtion for getting total budget of a department.

---
+++ prompts/getBudget.js
@@ -0,0 +1,44 @@
+const inquirer = require('inquirer');
+
+module.exports = (connection) => {
+    const query = `SELECT employee.first_name, employee.last_name, employee.role_id, role.id, role.salary, department.id, department.department_name
+    FROM employee
+    JOIN role ON employee.role_id = role.id
+    join department on role.department_id = department.id; SELECT * FROM department`;
+    connection.query(query, (err, results) => {
+        if (err) throw err;
+        const allData = results[0];
+        const allDepartments = results[1]
+        console.table(allData);
+        console.table(allDepartments);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'department_name',
+                message: 'What departments budget would tou like to view?',
+                choices: function() {
+                    const departmentList = [];
+                    for(let i = 0; i < allDepartments.length; i++) {
+                        departmentList.push(allDepartments[i].department_name);
+                    }
+                    return departmentList;
+                }
+            }
+        ]).then(response => {
+            console.log(response.department_name);
+            let counter = 0;
+            allData.forEach(element => {
+                if(element.department_name === response.department_name) {
+                    counter += element.salary
+                }
+            });
+            let message = `The total budget for: ${response.department_name} is ${counter}`;
+            console.log(message);
+            console.log('\n');
+            require('./start')(connection);
+            
+        })
+        
+        
+    })
+}
